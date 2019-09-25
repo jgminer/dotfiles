6a5231ca9058a0f9f10e1c6e047e90aac5d738b2
jgminer <jack.miner@outcomehealth.com>
2019-09-25 00:49:01 -0500

    Fun WIP

--- learning/test.py
+++ learning/test.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 
+import os
 import csv
 import requests
 
+all_resorts = []
+mapquest_key = os.environ.get('MAPQUEST_KEY')
+
 def get_loc(name, address):
-  print('getting location for {name}'.format(name))
+  print(f'getting location for {name}')
+  params = {'key': mapquest_key, 'location': address}
+  res = requests.get('http://open.mapquestapi.com/geocoding/v1/address', params=params)
+  print(res.json()['results'][0]['locations'][0]['latLng'])
 
+def open_csv():
+    with open('epic-local-locations.csv') as open_file:
+        reader = csv.DictReader(open_file, delimiter = ',', quotechar = '"')
+        for row in reader:
+            all_resorts.append(row)
 
-with open('epic_local_locations.csv') as open_file:
-    reader = csv.reader(open_file, delimiter = ',', quotechar = '"')
-    for row in reader:
-        print(row)
+def get_all_locations():
+    open_csv()
+    for resort in all_resorts:
+        print(get_loc(resort['Name'], resort['Address']))
+
+if __name__ == '__main__':
+    get_all_locations()
